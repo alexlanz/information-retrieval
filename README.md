992e8ad267a81b0f3facc38c5ac23d0ed657f72e
Alex Lanz <alex@krumer.it>
2014-11-13 16:35:24 +0100

    Cleaning up the project

--- README.md
+++ README.md
@@ -1,29 +1,3 @@
 Information Retrieval
 =====================
 
-### Installation
-
-```bash
-sudo pip3 install -U numpy scipy scikit-learn
-sudo pip3 install -U nltk
-```
-
-#### NLTK Ressources Installation:
-
-```bash
-python3
->>> import nltk
->>> nltk.download()
-```
-
-Models > punkt > Download
-
-### Execution
-
-```bash
-python3 main.py
-```
-
-### Books
-
-All books used during the implementation can be found here: https://github.com/alexlanz/information-retrieval/
--- vagrant/provision.sh
+++ vagrant/provision.sh
@@ -4,33 +4,5 @@
 sudo apt-get update
 sudo apt-get upgrade -y
 
-# Install python module requirements
-sudo apt-get install -y python3-numpy python3-scipy
-
 # Install pip
-sudo apt-get install -y python3-pip
-
-# Install pip packages
-#sudo pip3 install -U numpy
-#sudo pip3 install -U scipy
-sudo pip3 install -U scikit-learn
-sudo pip3 install -U nltk
-sudo pip3 install -U elasticsearch
-
-# Download nltk packages
-python3 -m nltk.downloader stopwords
-python3 -m nltk.downloader wordnet
-python3 -m nltk.downloader punkt
-
-# Install Java Runtime
-sudo apt-get install openjdk-7-jre-headless -y
-
-# Add Sources
-sudo wget -O - http://packages.elasticsearch.org/GPG-KEY-elasticsearch | sudo apt-key add -
-echo 'deb http://packages.elasticsearch.org/elasticsearch/1.3/debian stable main' | sudo tee -a /etc/apt/sources.list
-
-# Install ElasticSearch
-sudo apt-get update
-sudo apt-get install elasticsearch
-sudo update-rc.d elasticsearch defaults 95 10
-sudo /etc/init.d/elasticsearch start
+sudo apt-get install -y python-pip
