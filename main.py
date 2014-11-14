d48f50618c1f3692757000c80d852c965b02de99
Alex Lanz <alex@krumer.it>
2014-11-14 15:52:24 +0100

    Cleaning up the project

---
+++ main.py
@@ -0,0 +1,9 @@
+import snap
+
+v = snap.TIntV()
+
+v.Add(1)
+v.Add(2)
+v.Add(3)
+
+print v.Len()
--- vagrant/provision.sh
+++ vagrant/provision.sh
@@ -6,3 +6,12 @@ sudo apt-get upgrade -y
 
 # Install pip
 sudo apt-get install -y python-pip
+
+# Install Snap.py
+wget http://snap.stanford.edu/snappy/release/snap-1.1-2.3-centos6.5-x64-py2.6.tar.gz
+tar zxvf snap-1.1-2.3-centos6.5-x64-py2.6.tar.gz
+cd snap-1.1-2.3-centos6.5-x64-py2.6/
+sudo python setup.py install
+cd ..
+sudo rm -rf snap-1.1-2.3-centos6.5-x64-py2.6
+sudo rm -rf snap-1.1-2.3-centos6.5-x64-py2.6.tar.gz
