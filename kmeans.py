cba2a0956c868448ad59a6083b332750ed5ae206
Alex Lanz <alex@krumer.it>
2014-12-03 11:17:56 +0100

    Fuck it

--- kmeans.py
+++ kmeans.py
@@ -1,9 +1,7 @@
 from __future__ import print_function
-
 from sklearn.datasets import fetch_20newsgroups
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn import metrics
-
 from sklearn.cluster import KMeans
 from time import time
 
