3bd78d674f280c72ebd953f564729e1d0efe4e2a
Alex Lanz <alex@krumer.it>
2014-12-19 09:45:35 +0100

    Reseting repository

--- main.py
+++
@@ -1,5 +0,0 @@
-from sklearn.datasets import fetch_20newsgroups
-newsgroups_train = fetch_20newsgroups(subset="train")
-
-from pprint import pprint
-pprint(list(newsgroups_train.target_names))
--- vagrant/provision.sh
+++ vagrant/provision.sh
@@ -6,12 +6,3 @@ sudo apt-get upgrade -y
 
 # Install pip
 sudo apt-get install -y python-pip python3-pip
-
-# Install python module requirements
-sudo apt-get install -y python3-numpy python3-scipy python3-matplotlib
-
-# Install pip packages
-sudo pip3 install -U numpy
-sudo pip3 install -U scipy
-sudo pip3 install -U scikit-learn
-sudo pip3 install -U nltk
