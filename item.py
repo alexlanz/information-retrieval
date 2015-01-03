077c2840a9aad6e19afd8b54d7e9d33c43fb6b6a
Alex Lanz <alex@krumer.it>
2015-01-03 11:23:16 +0100

    Items

---
+++ item.py
@@ -0,0 +1,13 @@
+class Items:
+
+    def addItem(self, item, day):
+        print(item)
+
+    def getCountOfItems(self):
+        return 0
+
+    def getPositionOfItem(self, item):
+        return 0
+
+    def getCountOfViewsForItemWithin7Days(self, item, endDay):
+        return 0
