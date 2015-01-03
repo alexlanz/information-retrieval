716b8e68d107445e056df0edcd2895350a4e1455
Clemens Banas <clemens.banas@me.com>
2015-01-03 12:01:34 +0100

    modified interface

--- session.py
+++ session.py
@@ -1,23 +1,26 @@
 class Session:
 
     id = None
+    date = None
     duration = 0               # Integer in seconds
     numberOfClicks = 0
-    numberOfItems = 0
     viewedItems = []
-    special = False
     buy = False
 
-    def __init__(self, id, duration, numberOfClicks, numberOfItems, special, buy):
+    def __init__(self, id, date, duration, numberOfClicks, buy):
         self.id = id
+        self.date = date
         self.duration = duration
         self.numberOfClicks = numberOfClicks
-        self.numberOfItems = numberOfItems
-        self.special = special
         self.buy = buy
 
     def getVector(self):
-        return [self.duration, self.numberOfClicks, self.numberOfItems, (1 if self.special else 0)]
+        return [self.duration, self.numberOfClicks, self.numberOfItems, (1 if self.special else 0)] + self.getWeightedItemVector()
+    
+    def getWeightedItemVector(self):
+        arr = []
+        #todo
+        return arr
 
     def isBuyingEvent(self):
         return self.buy
